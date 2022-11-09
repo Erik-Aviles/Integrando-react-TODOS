58f1313e3347b9295432ad731df7a4fc4ea36533
Erika <erik8822@hotmail.com>
2022-11-08 21:38:58 -0500

    Cambiando la forma de cargar la ruta (new TODOS)

--- src/components/routes/new/NewPage.js
+++ src/components/routes/new/NewPage.js
@@ -4,15 +4,21 @@ import { useTodos } from '../useTodos';
 
 const NewPage = () => {
   const {states, stateUpdaters} = useTodos();
-  const {totalTodos} = states;
+  const {loading, totalTodos} = states;
   const {addTodo} = stateUpdaters;
-  return (
-    <TodoFormulario
-      submitTitle='Escribe tu nueva tarea'
-      submitText='Agregar'
-      numberTarea={totalTodos +1}
-      submitEvent={addTodo}/>
-  );
+
+  if (loading) {
+    return <p>Cargando ...</p>
+  } else {
+    return (
+      <TodoFormulario
+        submitTitle='Escribe tu nueva tarea'
+        submitText='Agregar'
+        numberTarea={totalTodos+1}
+        submitEvent={addTodo}
+      />
+    );
+  }
 }
 
 export { NewPage };
